9488052a05c888d587966fc55cc4495ab7fd13c3
Taiyo Sogawa <taiyosogawa@gmail.com>
2013-02-06 15:43:36 -0600

    submission for v2

--- index.php
+++ index.php
@@ -15,21 +15,21 @@
 -->
 
 
-	<svg id="container-div">
-    	<g>
-		<clipPath id="hex-mask">
-		<polygon points="270,0 0,160 0,485 270,645 560,485 560,160" />
-		</clipPath>
-		</g>
+	<div id="container-div">
+    	
+		<!--<polygon points="270,0 0,160 0,485 270,645 560,485 560,160" /> -->
+		
 
-		<a xlink:href="http://asbnu.org">
-		<image clip-path="url(#hex-mask)" height="100%" width="100%" xlink:href="img/bus_stop.png" />
-		</a>
+<!--
+		<g><clippath id='#hex-mask'>
+		<path style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 0.7; cursor: pointer;" fill="none" stroke="#ffffff" d="M0,0M0,-30A30,30,0,0,1,21.213203435596423,-21.213203435596423A30,30,0,0,1,30,0A30,30,0,0,1,21.213203435596423,21.213203435596423A30,30,0,0,1,0,30A30,30,0,0,1,-21.213203435596423,21.213203435596423A30,30,0,0,1,-30,0A30,30,0,0,1,-21.213203435596423,-21.213203435596423A30,30,0,0,1,0,-30Z" opacity="0.7" stroke-width="1.2" transform="matrix(1,0,0,1,383,186)"></path>
+		</clippath></g>
 
-	</svg>
+	-->
+	</div>
 
     <!-- Load all of the appropriate javascript -->
-
+    <!--<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>-->
     <script type="text/javascript" src="js/raphael.js"></script>
     <script type="text/javascript" src="js/portfolio.js"></script>
 </body>
--- js/portfolio.js
+++ js/portfolio.js
@@ -15,23 +15,26 @@ window.onload = function () {
         'cursor':'pointer'
     });
 
-    fElement['baby'] = makeCircle(bigNode.x - 200, bigNode.y, 30, {
-        fill: 'url(img/bus_stop.png)',
+    /*
+    fElement['image'] = r.image('img/bus_stop.png', 10, 10, 800, 800).attr({
+        path: circlePath(40)});
+    */
+   // fElement['image'].node.setAttribute('clip-path', 'url(#hex-mask)');
+
+    fElement['baby'] = makeCircle(bigNode.x + 200, bigNode.y, 30, {
+        fill: BLUE,
         opacity: .7,
         stroke: '#fff',
         'stroke-width': 1.2,
         'cursor': 'pointer'
     });
 
-    fElement['image'] = r.image('img/bus_stop.png', 10, 10, 800, 800).attr({
-        path: circlePath(40)});
+    //fElement['baby'].node.setAttribute('id', 'baby-node');
 
-    console.log(fElement['image'].node.attributes);
-    fElement['image'].node.setAttribute('clip-path', 'url(#hex-mask)');
-
-
-
-    
+/*
+    $('#baby-node').before('<g><clipPath id="hex-mask">');
+    $('#baby-node').after('</clipPath></g>');
+*/
     // Functions for dragging paths
     var start = function () {
         this.odx = 0;
@@ -56,9 +59,12 @@ window.onload = function () {
     selectProject = function () {
         this.toFront();
         var newPath;
+
+        var newOpacity;
         if(+(nodeState[this] = !nodeState[this])){
             bigNode.undrag();
             newPath = RRectPath(15 - this.x, 25 - this.y, window.innerWidth - 95, window.innerHeight - 50, 8, 8);
+            newOpacity = .9;
             fElement['glow'].hide();
             createProject();
             
@@ -66,48 +72,98 @@ window.onload = function () {
         } else {
             bigNode.drag(move, start, up);
             newPath = this.cpath;
+            newOpacity = .7;
             removeProject();
             window.setTimeout(fElement['glow'].hide(), DISP_TIME);
             window.setTimeout(bigNode.toFront, DISP_TIME);
         }
         this.stop().animate({
             path: newPath,
-            fill: BLUE,
-            opacity: .9}, DISP_TIME, 'easeIn');
+            opacity: newOpacity}, DISP_TIME, 'easeIn');
     };
     
     bigNode.drag(move, start, up);
-    bigNode.dblclick(selectProject);
+    //bigNode.dblclick(selectProject);
     bigNode.hover(glowOn, glowOff);
 
     for(f in fElement){
         fElement[f].click(selectProject);
-        fElement[f].hover(glowOn, glowOff);
+       // fElement[f].hover(glowOn, glowOff);
     }
 
 
     function createProject() {
-        pElement['title'] = r.text(740, 80, 'Bus  Stop').attr({
+        pElement['title'] = r.text(860, 100, 'Bus  Stop').attr({
             'font-family': 'bebas',
             'font-size': 72,
             'text-anchor': 'start',
             'fill': '#fff'
         });
-        pElement['image-frame'] = r.rect(45, 50, 666, 495).attr({
+        pElement['img-frame'] = r.rect(45, 70, 795, 492).attr({
             'stroke-width': 4,
             'stroke-linejoin': 'round',
             'stroke': '#222'
         });
-        pElement['image'] = r.image('img/bus_stop.png', 45, 50, 666, 495);
+        
+        pElement['img-2'] = r.image('img/bus_stop_2.png', 45, 70, 795, 492);
+        pElement['img-3'] = r.image('img/bus_stop_3.png', 45, 70, 795, 492);
+        pElement['img-4'] = r.image('img/bus_stop_4.png', 45, 70, 795, 492);
+        pElement['img-5'] = r.image('img/bus_stop_5.png', 45, 70, 795, 492);
+        pElement['img-1'] = r.image('img/bus_stop_1.png', 45, 70, 795, 492);
+
+        var textAttr = {
+            'font-family': 'arial',
+            'font-size': 16,
+            'text-anchor': 'start',
+            'fill': '#bbb',
+            'cursor': 'pointer'
+        };
+        pElement['story-1'] = r.text(860, 200, 
+            'I was searching for a way to make a bus stop\na desireable place to be.').attr(textAttr);
+        pElement['story-2'] = r.text(860, 270, 
+            'A meditative enclosing would be relaxing...\nbut who might you meet inside?').attr(textAttr);
+        pElement['story-3'] = r.text(860, 330, 
+            'I opted for a design that makes being in the elements a joy.').attr(textAttr);
+        pElement['story-4'] = r.text(860, 390, 
+            'The bus stop became a self sustained ecosphere which\npowers its own display and collects water for its plants.').attr(textAttr);
+        pElement['story-5'] = r.text(860, 460, 
+            'The community is empowered with a display of\nconcerted environmental efforts.').attr(textAttr);
+
+        pElement['story-1'].img = 'img-1';
+        pElement['story-2'].img = 'img-2';
+        pElement['story-3'].img = 'img-3';
+        pElement['story-4'].img = 'img-4';
+        pElement['story-5'].img = 'img-5';
+
+
+        pElement['story-1'].attr({'fill':'#fff'});
+        pElement['story-1'].hover(onTxt, offTxt);
+        pElement['story-2'].hover(onTxt, offTxt);
+        pElement['story-3'].hover(onTxt, offTxt);
+        pElement['story-4'].hover(onTxt, offTxt);
+        pElement['story-5'].hover(onTxt, offTxt);
+        
         for(p in pElement) {
             pElement[p].attr({opacity: 0});
         }
+
         window.setTimeout(showProject, DISP_TIME);
+    }
+
+    function onTxt() {
+        pElement[this.img].toFront();
+        pElement['story-1'].stop().animate({'fill': '#bbb'}, 400, 'easeIn');
+        this.stop().animate({'fill': '#fff'}, 400, 'easeIn');
+    }
+
+    function offTxt() {
+        this.stop().animate({'fill': '#bbb'}, 400, 'easeIn');
     }
 
     function showProject() {
         for(p in pElement) {
             pElement[p].animate({opacity: 1}, DISP_TIME / 2, 'easeIn');
+
         }
     }
 
